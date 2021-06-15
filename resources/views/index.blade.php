22801c3fda4c675873543ce5f256e020b41d199c
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-15 18:25:23 -0400

    Completando la estructura

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+@foreach($posts as $post)
+    <div>
+        <h2>{{ $post->title }}</h2>
+    </div>
+@endforeach
+{{ $posts->links() }}
+</body>
+</html>
