1ff8c298df964a4a211136b4081fce89232e4d73
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-15 18:46:04 -0400

    Mejorando el aspecto de la vista

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function getExcerptAttribute()
+    {
+        return Str::substr($this->content,0,140);
+    }
+    public function getPublishedAtAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,21 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
-<body>
-@foreach($posts as $post)
-    <div>
-        <h2>{{ $post->title }}</h2>
+<body class="bg-gray-100 text-gray-700">
+<div class="container mx-auto px-4">
+    <div class="grid grid-cols-3 my-10 ">
+        @foreach($posts as $post)
+            <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                <p class="text-sm">{{ $post->excerpt }}</p>
+                <p class="text-xs text-right">{{ $post->published_at }}</p>
+            </div>
+        @endforeach
+        <div class="my-10">
+            {{ $posts->links() }}
+
+        </div>
     </div>
-@endforeach
-{{ $posts->links() }}
+</div>
 </body>
 </html>
