0a3de46a7893df09fb219a3e7f8c171765166d59
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-22 09:04:06 -0400

    Instalación de laravel sanctum y protección de rutas api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use \App\Http\Controllers\Api\V1\PostController as PostV1;
 use \App\Http\Controllers\Api\V2\PostController as PostV2;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,7 +23,9 @@ use \App\Http\Controllers\Api\V2\PostController as PostV2;
 
 
 Route::apiResource('v1/posts', PostV1::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('v2/posts', PostV2::class)
-    ->only(['index', 'show']);
+    ->only(['index', 'show'])
+    ->middleware('auth:sanctum');
